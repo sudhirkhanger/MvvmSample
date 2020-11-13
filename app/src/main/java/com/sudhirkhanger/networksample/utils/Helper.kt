54d8a6dc8595800386046b1c48bcb5c72e83df70
Sudhir Khanger <sudhir@sudhirkhanger.com>
2020-11-13 09:16:12 +0530

    combine livedata

--- app/src/main/java/com/sudhirkhanger/networksample/utils/Helper.kt
+++ app/src/main/java/com/sudhirkhanger/networksample/utils/Helper.kt
@@ -2,6 +2,8 @@ package com.sudhirkhanger.networksample.utils
 
 import android.view.View
 import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import com.google.android.material.snackbar.Snackbar
 
 fun View.showSnackbar(
@@ -13,3 +15,21 @@ fun View.showSnackbar(
     @StringRes messageRes: Int,
     length: Int = Snackbar.LENGTH_LONG
 ) = Snackbar.make(this, resources.getString(messageRes), length).show()
+
+fun <T1, T2> combineTuple(
+    f1: LiveData<T1>,
+    f2: LiveData<T2>
+): LiveData<Pair<T1?, T2?>> =
+    MediatorLiveData<Pair<T1?, T2?>>().also { mediator ->
+        mediator.value = Pair(f1.value, f2.value)
+
+        mediator.addSource(f1) { t1: T1? ->
+            val (_, t2) = mediator.value!!
+            mediator.value = Pair(t1, t2)
+        }
+
+        mediator.addSource(f2) { t2: T2? ->
+            val (t1, _) = mediator.value!!
+            mediator.value = Pair(t1, t2)
+        }
+    }
